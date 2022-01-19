c7537083af4d38e3b433a7ce0436ec6edf9920a4
Transistor427 <427departament@gmail.com>
2022-01-19 15:38:24 +0300

    Update network.py

--- panels/network.py
+++ panels/network.py
@@ -55,6 +55,7 @@ class NetworkPanel(ScreenPanel):
         sbox = Gtk.Box()
         sbox.set_hexpand(True)
         sbox.set_vexpand(False)
+        sbox.set_halign(Center)
         sbox.add(self.labels['interface'])
         # sbox.add(self.labels['disconnect'])
 
