05c10ab360cce412a7c7d3f8977990ec3e5dd663
DmitriyAD <alexandrovi4dd@gmail.com>
2022-05-11 15:02:56 +0300

    test

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -53,7 +53,10 @@ class BasePanel(ScreenPanel):
         self.control['estop'] = self._gtk.ButtonImage('emergency', None, None, 1)
         self.control['estop'].connect("clicked", self.emergency_stop)
         self.control['shutdown'] = self._gtk.ButtonImage('shutdown', None, None, 1)
-        self.control['shutdown'].connect("clicked", self.shutdown)
+        # self.control['shutdown'].connect("clicked", self.shutdown)
+        _ = self.lang.gettext
+        self.control['shutdown'].connect("clicked", self._screen._confirm_send_action,
+                       _("Are you sure you wish to reboot the system?"), self.shutdown)
         self.control['wifi'] = self._gtk.ButtonImage('network', None, None, 1)
         self.control['wifi'].connect("clicked", self.menu_item_clicked, "network",{
                 "name": "Network",
