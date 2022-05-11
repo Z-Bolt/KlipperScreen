e0f618e389648431da5c158d49739c030241e403
DmitriyAD <alexandrovi4dd@gmail.com>
2022-05-11 13:20:48 +0300

    Remove reset icons

--- panels/system.py
+++ panels/system.py
@@ -21,21 +21,7 @@ class SystemPanel(ScreenPanel):
         grid = self._gtk.HomogeneousGrid()
         grid.set_row_homogeneous(False)
 
-        restart = self._gtk.ButtonImage('refresh', "\n".join(_('Klipper Restart').split(' ')), 'color1')
-        restart.connect("clicked", self.restart_klippy)
-        restart.set_vexpand(False)
-        firmrestart = self._gtk.ButtonImage('refresh', "\n".join(_('Firmware\nRestart').split(' ')), 'color2')
-        firmrestart.connect("clicked", self.restart_klippy, "firmware")
-        firmrestart.set_vexpand(False)
-
-        reboot = self._gtk.ButtonImage('refresh', _('System\nRestart'), 'color3')
-        reboot.connect("clicked", self._screen._confirm_send_action,
-                       _("Are you sure you wish to reboot the system?"), "machine.reboot")
-        reboot.set_vexpand(False)
-        # shutdown = self._gtk.ButtonImage('shutdown', _('System\nShutdown'), 'color4')
-        # shutdown.connect("clicked", self._screen._confirm_send_action,
-        #                  _("Are you sure you wish to shutdown the system?"), "machine.shutdown")
-        # shutdown.set_vexpand(False)
+        
 
         scroll = Gtk.ScrolledWindow()
         scroll.set_property("overlay-scrolling", False)
@@ -82,10 +68,6 @@ class SystemPanel(ScreenPanel):
         scroll.add(infogrid)
 
         grid.attach(scroll, 0, 0, 4, 2)
-        grid.attach(restart, 3, 2, 1, 1)
-        grid.attach(firmrestart, 1, 2, 1, 1)
-        grid.attach(reboot, 2, 2, 1, 1)
-        # grid.attach(shutdown, 3, 2, 1, 1)
         self.content.add(grid)
 
     def activate(self):
