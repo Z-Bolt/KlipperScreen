6d25740e7f804da1878ada9e939b44d0cd992a22
DmitriyAD <alexandrovi4dd@gmail.com>
2022-05-11 13:20:04 +0300

    test1

--- panels/updates.py
+++ panels/updates.py
@@ -38,12 +38,12 @@ class SystemPanel(ScreenPanel):
         # shutdown.set_vexpand(False)
 
 
-        grid.attach(restart, 3, 2, 1, 1)
-        grid.attach(firmrestart, 1, 2, 1, 1)
-        grid.attach(reboot, 2, 2, 1, 1)
+        grid.attach(restart, 3, 2, 0, 0)
+        grid.attach(firmrestart, 1, 2, 0, 0)
+        grid.attach(reboot, 2, 2, 0, 0)
         # grid.attach(shutdown, 3, 2, 1, 1)
         self.content.add(grid)
-        
+
     def restart_klippy(self, widget, type=None):
         if type == "firmware":
             self._screen._ws.klippy.restart_firmware()
