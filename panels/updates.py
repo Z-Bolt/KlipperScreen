6186d6d029257d1706064775f21e05d8bef8b0a4
DmitriyAD <alexandrovi4dd@gmail.com>
2022-05-11 13:39:59 +0300

    test1

--- panels/updates.py
+++ panels/updates.py
@@ -38,9 +38,9 @@ class SystemPanel(ScreenPanel):
         # shutdown.set_vexpand(False)
 
 
-        grid.attach(restart, 3, 5, 2, 1)
-        grid.attach(firmrestart, 1, 5, 2, 1)
-        grid.attach(reboot, 2, 5, 2, 1)
+        grid.attach(restart, 3, -1, 1, 1)
+        grid.attach(firmrestart, 1, -1, 1, 1)
+        grid.attach(reboot, 2, -1, 1, 1)
         # grid.attach(shutdown, 3, 2, 1, 1)
         self.content.add(grid)
         
