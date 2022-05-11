7bef8dc144e565823ce0fcfb363ef8ea42d53f0e
DmitriyAD <alexandrovi4dd@gmail.com>
2022-05-11 15:13:19 +0300

    Update icon shutdown

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -56,7 +56,7 @@ class BasePanel(ScreenPanel):
         # self.control['shutdown'].connect("clicked", self.shutdown)
         _ = self.lang.gettext
         self.control['shutdown'].connect("clicked", self._screen._confirm_send_action,
-                       _("Are you sure you wish to reboot the system?"), "machine.shutdown")
+                       _("Are you sure you wish to shutdown the system?"), "machine.shutdown")
         self.control['wifi'] = self._gtk.ButtonImage('network', None, None, 1)
         self.control['wifi'].connect("clicked", self.menu_item_clicked, "network",{
                 "name": "Network",
