a702c3718a8b1eefa1fa86c3b494210f5d4b963c
DmitriyAD <Alexandrovi4dd@gmail.com>
2022-05-12 16:50:31 +0300

    shutdown

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -55,7 +55,8 @@ class BasePanel(ScreenPanel):
         self.control['estop'].connect("clicked", self.emergency_stop)
         _ = self.lang.gettext
         self.control['shutdown'] = self._gtk.ButtonImage('shutdown', None, None, 1)
-        self.control['shutdown'].connect ( "clicked", self._screen._confirm_send_action, _("Are you sure you wish to shutdown the system?"),  {"script":"M81"})
+        # self.control['shutdown'].connect ( "clicked", self._screen._confirm_send_action, _("Are you sure you wish to shutdown the system?"),  {"script":"M81"})
+        self.control['shutdown'].connect ( "clicked", self.shutdown)
         self.control['wifi'] = self._gtk.ButtonImage('network', None, None, 1)
         self.control['wifi'].connect("clicked", self.menu_item_clicked, "network",{
                 "name": "Network",
