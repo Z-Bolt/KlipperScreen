41f02dca2f0808c9a2d1c9dcc9f8f4933a278e30
DmitriyAD <alexandrovi4dd@gmail.com>
2022-05-18 13:59:16 +0300

    test update image

--- ks_includes/KlippyGtk.py
+++ ks_includes/KlippyGtk.py
@@ -114,7 +114,7 @@ class KlippyGtk:
         return la
 
     def Image(self, image_name, scale=1):
-        filename = os.path.join(self.themedir, str(image_name) + ".svg")
+        filename = os.path.join(self.themedir, str(image_name) + ".svg", formatTemperatureString(0, 0))
         if os.path.exists(filename):
             pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(filename,
                                                              int(round(self.img_width * scale)),
