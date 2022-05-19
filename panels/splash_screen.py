263336e025a5181cef03225f9ad3a879a16ce17b
DmitriyAD <alexandrovi4dd@gmail.com>
2022-05-19 11:45:14 +0300

    back msg

--- panels/splash_screen.py
+++ panels/splash_screen.py
@@ -26,6 +26,7 @@ class SplashScreenPanel(ScreenPanel):
         image = self._gtk.Image("klipper", 10)
 
         self.labels['text'] = Gtk.Label(_("Initializing printer..."))
+
         self.labels['text'].set_line_wrap(True)
         self.labels['text'].set_line_wrap_mode(Pango.WrapMode.WORD_CHAR)
         self.labels['text'].set_halign(Gtk.Align.CENTER)
@@ -71,13 +72,8 @@ class SplashScreenPanel(ScreenPanel):
         self.content.add(main)
 
     def update_text(self, text):
-        if self.show_restart_buttons is not None:
-            text = "Klippers sanok"
-            self.labels['text'].set_markup("%s" % text)
-        else: 
-            text = "Klippers ept"
-            self.labels['text'].set_markup("%s" % text)   
-            self.show_restart_buttons()
+        self.labels['text'].set_markup("%s" % text)
+        self.show_restart_buttons()
 
     def clear_action_bar(self):
         for child in self.labels['actions'].get_children():
