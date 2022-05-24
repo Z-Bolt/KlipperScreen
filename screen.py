4dd45ee3396364cd473100e56c02dedce4f380a8
Transistor427 <427departament@gmail.com>
2022-05-24 14:02:44 +0300

    1

--- screen.py
+++ screen.py
@@ -941,8 +941,6 @@ class KlipperScreen(Gtk.Window):
    
     def _confirm_send_test(self, widget, response_id):
         if response_id == Gtk.ResponseType.OK:
-            widget.destroy() 
-            self._confirm_send_testimage
             self._ws.klippy.gcode_script("M81")
             os.system("sudo shutdown -P now")
         widget.destroy()
