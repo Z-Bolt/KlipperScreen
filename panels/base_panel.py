5953496c9c41fc6ea7364235f2d21862f491e86a
DmitryAD <alexandrovi4dd@gmail.com>
2022-05-30 15:04:05 +0300

    Update code style

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -419,5 +419,3 @@ class BasePanel(ScreenPanel):
             else:
                 self.control['time'].set_text(now.strftime("%I:%M %p"))
         return True
-    # def shutdown(self,widget):
-    #     self._screen._ws.klippy.gcode_script("M81")
