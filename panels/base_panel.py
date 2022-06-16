01410685cb10935049b2614ce778a938ebce9e73
DmitryAD <alexandrovi4dd@gmail.com>
2022-06-16 14:21:26 +0300

    ?

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -82,9 +82,9 @@ class BasePanel(ScreenPanel):
         else:
             self.control_grid.attach(self.control['estop'], 0, 5, 1, 2)
         if self._screen.vertical_mode:
-            self.control_grid.attach(self.control['shutdown'], 4, 0, 1, 1)
+            self.control_grid.attach(self.control['shutdown'], 4, 0, 1, 2)
         else:
-            self.control_grid.attach(self.control['shutdown'], 0, 4, 1, 1)    
+            self.control_grid.attach(self.control['shutdown'], 0, 4, 1, 2)    
         if self._screen.vertical_mode:
             self.control_grid.attach(self.control['wifi'], 3, 0, 1, 1)
         else:
