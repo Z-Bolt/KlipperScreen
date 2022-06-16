eaf4054cf98b76c4c19ade9f3e6c431affc05e6d
DmitriyAD <alexandrovi4dd@gmail.com>
2022-06-16 11:50:49 +0300

    create clean-nozzle buttonm

--- panels/extrude.py
+++ panels/extrude.py
@@ -43,6 +43,8 @@ class ExtrudePanel(ScreenPanel):
         self.labels['unload'].connect("clicked", self.load_unload, "-")
         self.labels['retract'] = self._gtk.ButtonImage("retract", _("Втянуть"), "color1")
         self.labels['retract'].connect("clicked", self.extrude, "-")
+        self.labels['nozzle'] = self._gtk.ButtonImage("clean-nozzle", _("Очистка"), "color3")
+        self.labels['nozzle'].connect("clicked", self.clean_nozzle)
         self.labels['temperature'] = self._gtk.ButtonImage("heat-up", _("Temperature"), "color4")
         self.labels['temperature'].connect("clicked", self.menu_item_clicked, "temperature", {
             "name":  _('Temperature'),
@@ -52,6 +54,7 @@ class ExtrudePanel(ScreenPanel):
         if i < 4:
             grid.attach(self.labels['temperature'], 3, 0, 1, 1)
         grid.attach(self.labels['extrude'], 0, 1, 1, 1)
+        grid.attach(self.labels['nozzle'], 1,0,1,1)
         if self.load_filament:
             grid.attach(self.labels['load'], 1, 1, 1, 1)
         if self.unload_filament:
@@ -180,6 +183,9 @@ class ExtrudePanel(ScreenPanel):
             self._screen._ws.klippy.gcode_script("UNLOAD_FILAMENT")
         if dir == "+":
             self._screen._ws.klippy.gcode_script("LOAD_FILAMENT")
+            
+    def clean_nozzle(self, widget):
+        self._screen._ws.klippy.gcode_script("CLEAN_NOZZLE")        
 
     def find_gcode_macros(self):
         macros = self._screen.printer.get_gcode_macros()
