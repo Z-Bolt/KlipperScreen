0b59a152fe4d33c5f7c28d16d0fc0a68ad20c0f2
DmitryAD <alexandrovi4dd@gmail.com>
2022-06-27 16:31:47 +0300

    Update text for cooldown

--- panels/preheat.py
+++ panels/preheat.py
@@ -106,7 +106,7 @@ class PreheatPanel(ScreenPanel):
     def set_temperature(self, widget, setting):
         _ = self.lang.gettext
         if setting == "cooldown":
-            self._screen.show_popup_message(_("Coldown"))
+            self._screen.show_popup_message(_("Принтер охлаждается"))
             for heater in self.active_heaters:
                 logging.info("Setting %s to %d" % (heater, 0))
                 if heater.startswith('heater_generic '):
