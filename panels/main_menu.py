b2e9e256aee2e33aa4bc73f68373f557a6949ecf
DmitryAD <alexandrovi4dd@gmail.com>
2022-07-21 11:08:01 +0300

    ok

--- panels/main_menu.py
+++ panels/main_menu.py
@@ -34,12 +34,11 @@ class MainPanel(MenuPanel):
         i = 0
         for x in self._printer.get_tools():
             self.labels[x] = self._gtk.ButtonImage("extruder-"+str(i), self._gtk.formatTemperatureString(0, 0))
-            self.labels[x].connect("clicked", self.on_popover_clicked) 
-            # "temperature", {
-            # "name":  _('Temperature'),
-            # "panel": "temperature"
+            self.labels[x].connect("clicked", self.menu_item_clicked, "temperature", {
+            "name":  _('Temperature'),
+            "panel": "temperature"
             
-            # })
+            })
             self.heaters.append(x)
             i += 1
 
@@ -47,12 +46,11 @@ class MainPanel(MenuPanel):
         for h in add_heaters:
             if h == "heater_bed":
                 self.labels[h] = self._gtk.ButtonImage("bed", self._gtk.formatTemperatureString(0, 0))
-                self.labels[h].connect("clicked", self.on_popover_clicked)
-                # "temperature", {
-                # "name":  _('Temperature'),
-                # "panel": "temperature"
+                self.labels[h].connect("clicked", self.menu_item_clicked, "temperature", {
+                "name":  _('Temperature'),
+                "panel": "temperature"
             
-                # })
+                })
             else:
                 name = " ".join(h.split(" ")[1:])
                 self.labels[h] = self._gtk.ButtonImage("heat-up", name)
@@ -86,91 +84,7 @@ class MainPanel(MenuPanel):
 
     def activate(self):
         return
-
-
-    def graph_show_device(self, widget, show=True):
-        self.labels['graph_settemp'] = self._gtk.Button(label=_("Set Temp"))
-        self.labels['graph_settemp'].connect("clicked", self.show_numpad)
-        self.labels['graph_hide'] = self._gtk.Button(label=_("Hide"))
-        self.labels['graph_hide'].connect("clicked", self.graph_show_device, False)
-        self.labels['graph_show'] = self._gtk.Button(label=_("Show"))
-        self.labels['graph_show'].connect("clicked", self.graph_show_device)
-
-        popover = Gtk.Popover()
-        self.labels['popover_vbox'] = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        popover.add(self.labels['popover_vbox'])
-        popover.set_position(Gtk.PositionType.BOTTOM)
-        self.labels['popover'] = popover
-        logging.info("Graph show: %s %s" % (self.popover_device, show))
-        self.labels['da'].set_showing(self.popover_device, show)
-        
-        
-        if show:
-            self.devices[self.popover_device]['name'].get_style_context().remove_class("graph_label_hidden")
-            self.devices[self.popover_device]['name'].get_style_context().add_class(
-                self.devices[self.popover_device]['class'])
-        else:
-            self.devices[self.popover_device]['name'].get_style_context().remove_class(
-                self.devices[self.popover_device]['class'])
-            self.devices[self.popover_device]['name'].get_style_context().add_class("graph_label_hidden")
-        self.labels['da'].queue_draw()
-        self.popover_populate_menu()
-        self.labels['popover'].show_all()
-
-    def hide_numpad(self, widget):
-        self.devices[self.active_heater]['name'].get_style_context().remove_class("button_active")
-        self.active_heater = None
-
-        if self._screen.vertical_mode:
-            self.grid.remove_row(1)
-            self.grid.attach(self.labels['menu'], 0, 1, 1, 1)
-        else:
-            self.grid.remove_column(1)
-            self.grid.attach(self.labels['menu'], 1, 0, 1, 1)
-        self.grid.show_all()
-
-    def on_popover_clicked(self, widget, device):
-        self.popover_device = device
-        po = self.labels['popover']
-        po.set_relative_to(widget)
-        self.popover_populate_menu()
-        po.show_all()
-
-    def popover_populate_menu(self):
-        pobox = self.labels['popover_vbox']
-        for child in pobox.get_children():
-            pobox.remove(child)
-
-        if self.labels['da'].is_showing(self.popover_device):
-            pobox.pack_start(self.labels['graph_hide'], True, True, 5)
-            if self.devices[self.popover_device]['type'] != "sensor":
-                pobox.pack_start(self.labels['graph_settemp'], True, True, 5)
-        else:
-            pobox.pack_start(self.labels['graph_show'], True, True, 5)
-            if self.devices[self.popover_device]['type'] != "sensor":
-                pobox.pack_start(self.labels['graph_settemp'], True, True, 5)
-    def show_numpad(self, widget):
-
-        if self.active_heater is not None:
-            self.devices[self.active_heater]['name'].get_style_context().remove_class("button_active")
-        self.active_heater = self.popover_device
-        self.devices[self.active_heater]['name'].get_style_context().add_class("button_active")
-
-        if "keypad" not in self.labels:
-            self.labels["keypad"] = Keypad(self._screen, self.change_target_temp, self.hide_numpad)
-        self.labels["keypad"].clear()
-
-        if self._screen.vertical_mode:
-            self.grid.remove_row(1)
-            self.grid.attach(self.labels["keypad"], 0, 1, 1, 1)
-        else:
-            self.grid.remove_column(1)
-            self.grid.attach(self.labels["keypad"], 1, 0, 1, 1)
-        self.grid.show_all()
-
-        self.labels['popover'].popdown()
                 
-
     def process_update(self, action, data):
         if action != "notify_status_update":
             return
