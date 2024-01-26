a78b57e63407840b4e731e1627c09fbbf0b173a6
Vlad <47721986+Transistor427@users.noreply.github.com>
2024-01-26 15:05:36 +0300

    Modify base_panel

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -116,8 +116,8 @@ class BasePanel(ScreenPanel):
             self.main_grid.attach(self.action_bar, 0, 2, 1, 1)
             # self.action_bar.set_orientation(orientation=Gtk.Orientation.VERTICAL)
             self.action_bar.set_orientation(orientation=Gtk.Orientation.HORIZONTAL)
-            self.main_grid.attach(self.titlebar, 1, 0, 1, 1)
-            self.main_grid.attach(self.content, 1, 1, 1, 1)
+            self.main_grid.attach(self.titlebar, 0, 0, 1, 1)
+            self.main_grid.attach(self.content, 0, 1, 1, 1)
 
         # Layout is and content are on screen_panel
         self.layout.add(self.main_grid)
