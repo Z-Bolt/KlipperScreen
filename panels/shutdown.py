58a31eb891c26e9036d832d67f0d3df76705effc
Vlad <427departament@gmail.com>
2025-02-06 14:08:15 +0300

    Добавлена функция скрытия кнопки "Заблокировать"

--- panels/shutdown.py
+++ panels/shutdown.py
@@ -33,18 +33,18 @@ class Panel(ScreenPanel):
         self.main = Gtk.Grid(row_homogeneous=True, column_homogeneous=True)
         if self._show_lock_button:
             if self._printer and self._printer.state not in {'disconnected', 'startup', 'shutdown', 'error'}:
-                self.main.attach(estop, 0, 0, 1, 1)
-            self.main.attach(restart_ks, 1, 0, 1, 1)
-            self.main.attach(poweroff, 0, 1, 1, 1)
-            self.main.attach(restart, 1, 1, 1, 1)
-            self.content.add(self.main)
-        else:
-            if self._printer and self._printer.state not in {'disconnected', 'startup', 'shutdown', 'error'}:
                 self.main.attach(estop, 1, 0, 1, 1)
             self.main.attach(restart_ks, 2, 0, 1, 1)
             self.main.attach(lock_screen,0, 0, 1, 2)
             self.main.attach(poweroff, 1, 1, 1, 1)
             self.main.attach(restart, 2, 1, 1, 1)
+            self.content.add(self.main)
+        else:
+            if self._printer and self._printer.state not in {'disconnected', 'startup', 'shutdown', 'error'}:
+                self.main.attach(estop, 0, 0, 1, 1)
+            self.main.attach(restart_ks, 1, 0, 1, 1)
+            self.main.attach(poweroff, 0, 1, 1, 1)
+            self.main.attach(restart, 1, 1, 1, 1)
             self.content.add(self.main)
 
     def reboot_poweroff(self, widget, method):
