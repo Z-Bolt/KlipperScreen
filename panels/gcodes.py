e249ebc9ff22db9f9ef34b8ff01acdf2cd8884c4
Vlad <427departament@gmail.com>
2025-12-25 17:55:39 +0300

    Refactor file transfer confirmation in gcodes.py to clarify internal storage transfer; update Russian localization for improved accuracy and new messages.

--- panels/gcodes.py
+++ panels/gcodes.py
@@ -320,25 +320,13 @@ class Panel(ScreenPanel):
         dest = "gcodes/" + filename
         params = {"source": f"{filepath}",
                   "dest": f"{dest}"}
-        gcodes_path = "/home/pi/printer_data/"
-        check_file = os.path.exists(gcodes_path + dest)
-        if check_file:
-            self._screen._confirm_send_action(
-                None,
-                _("A file with this name already exists") + "\n\n" +
-                _("You may be trying to move a file that is already in the main directory") +
-                "\n\n" + _("Replace it?") + "\n\n" + filepath,
-                "server.files.move",
-                params
-            )
-        else:
-            self._screen._confirm_send_action(
-                None,
-                _("This function is designed to move a file to the main directory") +
-                "\n\n" + _("Move file to main directory?") + "\n\n" + filepath,
-                "server.files.move",
-                params
-            )
+        self._screen._confirm_send_action(
+            None,
+            _("After confirmation, the file will be transferred to internal storage.") +
+            "\n\n" + _("Transfer file?") + "\n\n" + filepath,
+            "server.files.move",
+            params
+        )
 
     def confirm_delete_directory(self, widget, dirpath):
         logging.debug(f"Sending delete_directory {dirpath}")
